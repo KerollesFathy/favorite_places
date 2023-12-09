f689647f41a0ff41bec35e0f56f6e08c0da94596
KerollesFathy <kerolles.f@outlook.com>
2023-12-09 18:48:02 +0200

    added places screen & place list widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:favorite_places/screens/places.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -39,7 +40,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      home: ...,
+      home: const PlacesScreen(),
     );
   }
 }
