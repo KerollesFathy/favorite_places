f57b7fdbf9c83273aa8fbdc5a8877e4b384b9f59
KerollesFathy <kerolles.f@outlook.com>
2024-04-19 17:53:15 +0200

    install Image Picker package

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
     const AddPlaceScreen({super.key});
@@ -47,6 +48,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                                 color: Theme.of(context).colorScheme.onBackground,
                                 ),
                         ), 
+                    const SizedBox(height: 10),
+                    ImageInput(),
                     const SizedBox(height: 16),
                     ElevatedButton.icon(
                         onPressed: _savePlace,
