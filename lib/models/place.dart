dbf8ffb6f3bd9f3b815345e7b45df306eead9a5d
KerollesFathy <kerolles.f@outlook.com>
2024-05-31 20:05:41 +0300

    :sparkles: (ui) add places details screen

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,12 +1,16 @@
 import 'package:uuid/uuid.dart';
+import 'dart:io';
 
 const uuid = Uuid();
 
 class Place {
     Place({
-        required this.title
+        required this.title, this.image,
     }) : id = uuid.v4();
 
     final String id;
     final String title;
+    final String? image; // Making image parameter optional by using '?'
+
+
 }
--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,36 +1,53 @@
 import 'package:flutter/material.dart';
-
+import 'dart:io';
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/places_detail.dart';
 
 class PlacesList extends StatelessWidget {
-    const PlacesList({
-        super.key,
-        required this.places,
-    });
-    final List<Place> places;
+  const PlacesList({
+    Key? key,
+    required this.places,
+  }) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        if (places.isEmpty) {
-            return Center(
-                child: Text("No places added yet", 
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Theme.of(context).colorScheme.onBackground,
-                    ),
-                ),
-                
-            );
-        } 
+  final List<Place> places;
 
-        return ListView.builder(
-            itemCount: places.length,
-            itemBuilder: (ctx, index) => ListTile(
-                    title: Text(places[index].title, 
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.onBackground,
-                    ),)
-                ),
-            
-        );
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Text(
+          "No places added yet",
+          style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      );
     }
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (ctx, index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => PlaceDetailScreen(place: places[index]),
+              ),
+            );
+          },
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 26,
+              // backgroundImage: FileImage(places[index].image),
+            ),
+            title: Text(
+              places[index].title,
+              style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
